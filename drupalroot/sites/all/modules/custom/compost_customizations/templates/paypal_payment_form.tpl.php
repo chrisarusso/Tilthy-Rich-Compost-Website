459e8b8825e87c77516a75c72b9462bc69ecd6af
Chris Russo <chris.andrews.russo@gmail.com>
2012-09-04 12:56:47 -0400

    Add initial paypal block template

---
+++ drupalroot/sites/all/modules/custom/compost_customizations/templates/paypal_payment_form.tpl.php
@@ -0,0 +1,21 @@
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+  <input type="hidden" name="cmd" value="_s-xclick">
+  <input type="hidden" name="hosted_button_id" value="DGV69L4NQW9DU">
+  <table>
+    <tr><td><input type="hidden" name="on0" value="Monthly damage">Monthly damage</td></tr><tr><td><select name="os0">
+    <option value="Select">Please select your monthly contribution amount</option>
+    <option value="Carter level">Carter level : $5.00 USD - monthly</option>
+    <option value="Carter + $5">Carter + $5 : $10.00 USD - monthly</option>
+    <option value="Carter goes to middle school">Carter goes to middle school : $15.00 USD - monthly</option>
+    <option value="Middle of the road">Middle of the road : $20.00 USD - monthly</option>
+    <option value="Middle + $5">Middle + $5 : $25.00 USD - monthly</option>
+    <option value="One exclamation!">One exclamation! : $30.00 USD - monthly</option>
+    <option value="Golly, thanks!!">Golly, thanks!! : $35.00 USD - monthly</option>
+    <option value="Big spender!!!">Big spender!!! : $40.00 USD - monthly</option>
+    <option value="Please...?">Please...? : $1,000.00 USD - monthly</option>
+  </select> </td></tr>
+  </table>
+  <input type="hidden" name="currency_code" value="USD">
+  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+  <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
