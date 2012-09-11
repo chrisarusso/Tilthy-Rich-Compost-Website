341fc7c208773821d37c7f2201b77bed212e0681
Chris Russo <chris.andrews.russo@gmail.com>
2012-09-11 16:28:00 -0400

    Add testing paypal template and logic

---
+++ drupalroot/sites/all/modules/custom/compost_customizations/templates/sandbox_paypal_payment_form.tpl.php
@@ -0,0 +1,25 @@
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+ <input type="hidden" name="cmd" value="_s-xclick">
+  <input type="hidden" name="hosted_button_id" value="TUHBL248JXTTC">
+  <table>
+    <tr><td><input type="hidden" name="on0" value=""></td></tr><tr><td><select name="os0">
+      <option value="Option 1">Option 1 : $0.00 USD - monthly</option>
+      <option value="Option 2">Option 2 : $5.00 USD - monthly</option>
+      <option value="Option 3">Option 3 : $25.00 USD - monthly</option>
+    </select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="USD">
+<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+<!--
+
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+  <input type="hidden" name="cmd" value="_s-xclick">
+  <input type="hidden" name="hosted_button_id" value="XJD3NNMLNAQPJ">
+  <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_subscribe_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+  <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+-->
