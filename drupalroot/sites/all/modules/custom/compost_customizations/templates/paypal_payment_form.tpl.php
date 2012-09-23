877d6d731d17142d61043c55d74dc362e102600c
Chris Russo <chris.andrews.russo@gmail.com>
2012-09-23 02:14:58 -0400

    Update custom UID paypal submission variable
    Use logged in user id first, then try
    $_GET['uid'] next.  Also add in test variable
    uid to see if it's picked up in the return
    post from payapl
    
    Issue #6

--- drupalroot/sites/all/modules/custom/compost_customizations/templates/paypal_payment_form.tpl.php
+++ drupalroot/sites/all/modules/custom/compost_customizations/templates/paypal_payment_form.tpl.php
@@ -17,5 +17,6 @@
   <input type="hidden" name="currency_code" value="USD">
   <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
   <input type="hidden" name="custom" value="<?php print $uid; ?>">
+  <input type="hidden" name="uid" value="<?php print $uid; ?>">
   <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
