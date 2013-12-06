a35ee8d56fc9a14a66f094f52e07fc4ffc61ff5b
Chris Russo <chris.andrews.russo@gmail.com>
2013-12-06 11:36:57 -0500

    Move to stripe payment processor
    Remove outdated paypal customizations
      from compost customization
    Add Stripe PHP Library
    Add custom stripe integration module
    Update homepage template since user now lands
      there after registering and the set message
      was too hidden
    Update feature to add new user fields
    
    Issue #6
    Issue #37

---
+++ drupalroot/sites/all/modules/custom/stripe_integration/js/stripe_integration_checkout.js
@@ -0,0 +1,33 @@
+(function ($) {
+
+    var handler = StripeCheckout.configure({
+        key: Drupal.settings.stripeIntegration.publishableKey,
+        image: Drupal.settings.basePath + Drupal.settings.stripeIntegration.logoURL,
+        token: function(token, args) {
+            var form = $('#user-register-form');
+            var input = $("<input name='stripeToken' value='" + token.id + "' style='display:none;' />");
+            form.append(input[0]);
+            form.find('#edit-submit').attr('disabled','disabled');
+            form.submit();
+        }
+    });
+
+    if (typeof Drupal.settings.stripeIntegration.stripeToken === 'undefined') {
+        // variable is undefined
+
+        document.getElementById('edit-submit').addEventListener('click', function(e) {
+            // Open Checkout with further options
+            handler.open({
+                name: 'Tilthy Rich Compost',
+                description: 'Monthly Subscription',
+                amount: jQuery('#edit-field-monthly-contribution-und-0-value').val() * 100,
+                email: jQuery('#edit-mail').val()
+            });
+        e.preventDefault();
+        });
+    } else{
+        var form = $('#user-register-form');
+        var input = $("<input name='stripeToken' value='" + Drupal.settings.stripeIntegration.stripeToken + "' style='display:none;' />");
+        form.append(input[0]);
+    }
+}(jQuery));
