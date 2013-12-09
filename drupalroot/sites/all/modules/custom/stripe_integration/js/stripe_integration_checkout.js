678776dbd449127c4f1ffb83d3faa6dd64badf4c
Chris Russo <chris.andrews.russo@gmail.com>
2013-12-09 16:10:13 -0500

    Make stripe js work on reg and update page

--- drupalroot/sites/all/modules/custom/stripe_integration/js/stripe_integration_checkout.js
+++ drupalroot/sites/all/modules/custom/stripe_integration/js/stripe_integration_checkout.js
@@ -4,7 +4,7 @@
         key: Drupal.settings.stripeIntegration.publishableKey,
         image: Drupal.settings.basePath + Drupal.settings.stripeIntegration.logoURL,
         token: function(token, args) {
-            var form = $('#user-register-form');
+            var form = $('#user-register-form, #stripe-integration-payment-update-form');
             var input = $("<input name='stripeToken' value='" + token.id + "' style='display:none;' />");
             form.append(input[0]);
             form.find('#edit-submit').attr('disabled','disabled');
@@ -26,7 +26,7 @@
         e.preventDefault();
         });
     } else{
-        var form = $('#user-register-form');
+        var form = $('#user-register-form, #stripe-integration-payment-update-form');
         var input = $("<input name='stripeToken' value='" + Drupal.settings.stripeIntegration.stripeToken + "' style='display:none;' />");
         form.append(input[0]);
     }
