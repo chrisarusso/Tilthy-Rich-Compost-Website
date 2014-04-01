813f7c6cf8cf324166e613dce9b9874c7f9a8660
Chris Russo <chris.andrews.russo@gmail.com>
2014-04-01 05:05:43 -0400

    Add preprocessing, remove in template logic

--- drupalroot/sites/all/themes/custom/tilthyrich2/template.php
+++ drupalroot/sites/all/themes/custom/tilthyrich2/template.php
@@ -10,5 +10,41 @@ function tilthyrich2_theme($existing, $type, $theme, $path){
     'template' => 'templates/user-register',
     ),
   );
+}
 
+/**
+ * Implements hook_preprocess_page().
+ */
+function tilthyrich2_preprocess_page(&$variables) {
+  $variables['partners'] = tilthyrich2_get_partners();
+
+  $form = drupal_get_form('user_register_form');
+  $variables['registration_form'] = drupal_render($form);
+
+}
+
+/**
+ * @return array
+ *  Array of partners with necessary metadata to feature on homepage
+ */
+function tilthyrich2_get_partners() {
+  $qry = new EntityFieldQuery();
+
+  $result = $qry->entityCondition('entity_type', 'node')
+    ->entityCondition('bundle', 'partner')
+    ->propertyCondition('status', 1)
+    ->execute();
+
+  $nids = array_keys($result['node']);
+  $nodes = node_load_multiple($nids);
+
+  foreach ($nodes as $partner_node) {
+    $partners[] = array(
+      'title' => $partner_node->title,
+      'img' => image_style_url('thumbnail', $partner_node->field_icon[LANGUAGE_NONE][0]['uri']),
+      'url' => 'http://google.com',
+    );
+  }
+
+  return $partners;
 }
